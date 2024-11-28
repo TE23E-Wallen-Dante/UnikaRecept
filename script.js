737521298f830a950a19698a0325c23d855d5566
Dante Wallén <dante.wallen@elev.ntig.se>
2024-11-28 20:42:27 +0100

    varukorg test 2 - loggar allt, skriver ut allt, skriver också när varukorgen är full, lagt till tom betalningsknapp

--- script.js
+++ script.js
@@ -6,16 +6,15 @@ const max = 5;
 let count = 0;
 
 // Funktion som identifierar namn, pris, typ, och företag, och skriver ut det i konsolen
-function choose(namn, pris, typ, företag)
-{
+function choose(namn, pris, typ, företag) {
     console.log("Namn: " + namn + "\nPris: " + pris + "\nTyp: " + typ + "\nFöretag: " + företag)
 
     // Om count är mindre än max, lägg gör det under
-    if(count<max){
+    if (count < max) {
         // Skapa ett li-element med liknande innehåll som konsolen
         let li = document.createElement("li");
         li.textContent = namn + "; Pris: " + pris + " kr; Typ: " + typ + "; Företag: " + företag;
-        li.id = "buy";
+        li.id = "varukorg";
 
         // Kopplar ul-elementet med det nyskapade li, så att allt hamnar rätt (kommunikation)
         ul.appendChild(li);
@@ -23,22 +22,21 @@ function choose(namn, pris, typ, företag)
 
         console.log("Varukorgen är full!");
 
-    if(count=max){
-        // Skapa ett p-element med text som säger att varukorgen är full
-        let p = document.createElement("p");
-        p.textContent = "Varukorgen är full!";
-        p.id = "varukorg-full"
+        if (count >= max) {
+            // Skapa ett p-element med text som säger att varukorgen är full
+            let p = document.createElement("p");
+            p.textContent = "Varukorgen är full!";
+            p.id = "varukorg-full"
 
-        aside.appendChild(p);
+            aside.appendChild(p);
 
-    }
-        
+        }
+
     }
 };
 
-function fullVarukorg()
+function buy()
 {
     
-    
-}
+};
 
